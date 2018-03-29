7abcb15cdc7df8394b6139e4bb18cc431e5ee97a
Hugh <hug_hob@hotmail.com>
2018-03-29 15:33:38 +0100

    Added in a package intended to hold modelling code

---
+++ dublinbike/modelling/modelling.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sqlalchemy.engine import create_engine
+
+
+def main():
+    conex = create_engine("mysql+pymysql://root:Rugby_777@localhost/dublinbikes")
+    
+    df = pd.read_sql_table("data", conex)
+    
+    print(df)
+    
+    
+
+
+        
+        
