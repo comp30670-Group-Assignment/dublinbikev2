443964dc2594f59165351f42b2fd9e5534866ec1
Hugh <hug_hob@hotmail.com>
2018-03-31 16:04:49 +0100

    making edits to miner trying to get it working on pi

--- dublinbike/dublinbikeminer/dublinbikeminer.py
+++ dublinbike/dublinbikeminer/dublinbikeminer.py
@@ -1,45 +1,20 @@
 import requests
 import json
 import time
-import datetime
 
 # Import MySQl
 from mysql.connector import (connection, cursor)
-from gevent.libev.corecext import stat
 
 def main():
     
     # Connection to local database
-    conex = connection.MySQLConnection(user='root', password='Rugby_777', host='0.0.0.0', database='software_engineering')
+    conex = connection.MySQLConnection(user='hugh', password='password', host='0.0.0.0', database='dublinbikes')
     # MySQL object
     cursor = conex.cursor()
-
-    
-    number = 10
-    name = None
-    address = None
-    position_lat = 0.0
-    position_lng = 0.0
-    status = None
-    bike_stands = 0
-    available_bike_stands = 0
-    available_bikes = 0
-    last_update = 0
-    
-    # Execute test
-    sqlQuery = "INSERT INTO dublinBikes(Number, Name, Address, Position_Lat, Position_Lng, Status, Bike_Stands, Available_Bike_Stands, Available_bikes, Last_Update) \
-                VALUES('%d', '%s', '%s', '%f', '%f', '%s', '%d', '%d', '%d', '%d')" % \
-                (number, name, address, position_lat, position_lng, status, bike_stands, available_bike_stands, available_bikes, last_update)
-                
-    
-    #cursor.execute(sqlQuery)
-    
-    #conex.commit()
-    
     
     
     while True:
-        
+
         link = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&&apiKey=4dc48c410fefd7a42d52cdc4a9c6eb7ce0f67ae0"
         
         r = requests.get(link)
@@ -64,16 +39,21 @@ def main():
                 sqlQuery = "INSERT INTO dublinBikes(Number, Name, Address, Position_Lat, Position_Lng, Status, Bike_Stands, Available_Bike_Stands, Available_bikes, Last_Update) \
                 VALUES('%d', '%s', '%s', '%f', '%f', '%s', '%d', '%d', '%d', '%d')" % \
                 (number, name, address, position_lat, position_lng, status, bike_stands, available_bike_stands, available_bikes, last_update)
+                
+                cursor.execute(sqlQuery)
+                
+                cursor.commit()
         
                 pass
             except:
                 pass
             
         else:
+            print("api connection failed")
             # Error occurred 
             pass
         
-        return False
+        
     
         time.sleep(300)
 
