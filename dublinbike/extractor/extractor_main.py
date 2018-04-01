1f37d6edbb24ddd312e8b27451f06b029477b501
Hugh <hug_hob@hotmail.com>
2018-04-01 08:53:52 +0100

    began configuring extractor to extract and run calculation
    calculations are done once per day and pushed to a json file in static

---
+++ dublinbike/extractor/extractor_main.py
@@ -0,0 +1,32 @@
+from extractorv1 import *
+import time
+import json
+
+new_conex = Extractor()
+
+while True:
+    
+    fhb = open("/home/hugh/git/dublinbike/dublinbike/dublinbikeflask/static/av_bike.json", "w")
+    fhs = open("/home/hugh/git/dublinbike/dublinbike/dublinbikeflask/static/av_stand.json","w")
+    
+    new_conex.avg_available_bike()
+    new_conex.avg_available_stand()
+    
+    avg_bike = new_conex.avg_available_bike_dict
+    avg_stand = new_conex.avg_available_stand_dict
+    
+    avg_bike_json = json.dumps(avg_bike)
+    avg_stand_json = json.dumps(avg_stand)
+    
+    fhb.write(avg_bike_json)
+    
+    fhb.write(avg_stand_json)
+    
+    fhb.close
+    fhs.close
+    
+    time.sleep(86400)
+    
+    
+    
+    
