55e0c9b0b3d0eebfdc53639c1d914498f9a73cad
comp30670-Group-Assignment <37292988+comp30670-Group-Assignment@users.noreply.github.com>
2018-04-02 19:21:29 +0100

    Fixed listener event

--- dublinbike/dublinbikeflask/static/javascriptNew.js
+++ dublinbike/dublinbikeflask/static/javascriptNew.js
@@ -1,9 +1,9 @@
 function mapInit() {
-	var mapCenter = new google.maps.LatLng(53.3498,-6.2603);
-	var mapCanvas = document.getElementById("googleMap");
-	var mapOptions = {center: mapCenter, zoom: 14};
-	var map = new google.maps.Map(mapCanvas, mapOptions);
-	
+	var mapCenter = {lat: 53.3498, lng: -6.2603};
+    var map = new google.maps.Map(document.getElementById('googleMap'), {
+        zoom: 14, 
+        center: mapCenter});
+    
   	var xhttp = new XMLHttpRequest();
   	xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
@@ -17,11 +17,28 @@ function mapInit() {
       		}
       			
       	// Fill map up with markers based on json data
+        // Right now all markers are being generated, but they are being given all the same
+        // content, when they should be given content depending on their own personal info.
 	for(i = 0; i < index.length; i++) {
-      			
+    var mytext = index[i] + "<br> Station No.: " + "<br> Free bikes: ";
+    var myinfowindow = new google.maps.InfoWindow({
+    content: mytext});
+             
 	var position = new google.maps.LatLng(locations[index[i]].latitude, locations[index[i]].longitude);
-        marker = new google.maps.Marker({position: position, map: map});}
-		
+        
+    var marker = new google.maps.Marker({
+    position: position, 
+    map: map,
+    infowindow: myinfowindow
+    });
+        
+    google.maps.event.addListener(marker, 'click', function() {
+        this.infowindow.open(map, this);
+
+});}
+        
+            
+            
 	}};
   	xhttp.open("GET", "static/stations.json", true);
   	xhttp.send();
