d3a95d6900e85c6c8d6b9bcee8c3e6ffa034382a
neuJump <35955724+neuJump@users.noreply.github.com>
2018-04-04 19:44:12 +0100

    Basic framework for handling JSON.

--- dublinbike/dublinbikeflask/app.py
+++ dublinbike/dublinbikeflask/app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 #from extractor import *
 
 app = Flask(__name__)
+
+@app.route('/_add_numbers')
+def add_numbers():
+	#a = request.args.get('a', 0, type=int)
+	#b = request.args.get('b', 0, type=int)
+	a = 10;
+	b = 10;
+	return jsonify(result=a + b)
 
 #placeholder for current file
 @app.route('/')
