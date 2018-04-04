06762dfd6a0962dae69a707509584d038fc764b7
neuJump <35955724+neuJump@users.noreply.github.com>
2018-04-04 15:55:59 +0100

    Clean up code JavaScipt code

--- dublinbike/dublinbikeflask/static/javascriptNew.js
+++ dublinbike/dublinbikeflask/static/javascriptNew.js
@@ -9,35 +9,34 @@ function mapInit() {
     	if (this.readyState == 4 && this.status == 200) {
       		locations = JSON.parse(this.responseText);
       			
-      	// Create empty array 'index'
-	var index = [];
-	// Add (i.e. 'push') the next xID item to the array
-      	for(var xID in locations) {
-      		index.push(xID);		
+      		// Create empty array 'index'
+			var index = [];
+			// Add (i.e. 'push') the next xID item to the array
+      		for(var xID in locations) {
+      			index.push(xID);		
       		}
       			
-      	// Fill map up with markers based on json data
-        // Right now all markers are being generated, but they are being given all the same
-        // content, when they should be given content depending on their own personal info.
-	for(i = 0; i < index.length; i++) {
-    var mytext = index[i] + "<br> Station No.: " + "<br> Free bikes: ";
-    var myinfowindow = new google.maps.InfoWindow({
-    content: mytext});
+      		// Fill map up with markers based on json data
+        	// Right now all markers are being generated, but they are being given all the same
+        	// content, when they should be given content depending on their own personal info.
+			for(i = 0; i < index.length; i++) {
+    			var mytext = index[i] + "<br> Station No.: " + "<br> Free bikes: ";
+    			var myinfowindow = new google.maps.InfoWindow({content: mytext});
              
-	var position = new google.maps.LatLng(locations[index[i]].latitude, locations[index[i]].longitude);
+				var position = new google.maps.LatLng(locations[index[i]].latitude, locations[index[i]].longitude);
         
-    var marker = new google.maps.Marker({
-    position: position, 
-    map: map,
-    infowindow: myinfowindow
-    });
+    			var marker = new google.maps.Marker({
+    				position: position, 
+    				map: map,
+    				infowindow: myinfowindow
+    			});
         
-    google.maps.event.addListener(marker, 'click', function() {
-        this.infowindow.open(map, this);
+    		google.maps.event.addListener(marker, 'click', function() {
+        		this.infowindow.open(map, this);
 
-});}
-        
-            
+			});
+		
+		}      
             
 	}};
   	xhttp.open("GET", "static/stations.json", true);
