ffdfe3d73877255ba57a9be9aa92aebd94b272e9
comp30670-Group-Assignment <37292988+comp30670-Group-Assignment@users.noreply.github.com>
2018-04-08 09:44:21 +0100

    On marker click, pop-up window appears

--- dublinbike/dublinbikeflask/static/javascriptNew.js
+++ dublinbike/dublinbikeflask/static/javascriptNew.js
@@ -1,4 +1,11 @@
-function mapInit() {
+// Alasa's function to open up a new pop-up window when one of the markers is selected.
+// The URL needs to be changed to show the weather and station Google charts, etc.
+function poponclick(){
+    var testwindow = window.open("http://www.this-page-intentionally-left-blank.org/", "_blank",  "width=600,height=400");
+    testwindow.moveTo(400, 400);
+}
+  
+function mapInit() {    
 	var mapCenter = {lat: 53.3498, lng: -6.2603};
     var map = new google.maps.Map(document.getElementById('googleMap'), {
         zoom: 14, 
@@ -15,12 +22,17 @@ function mapInit() {
       		for(var xID in locations) {
       			index.push(xID);		
       		}
-      			
+          
       		// Fill map up with markers based on json data
         	// Right now all markers are being generated, but they are being given all the same
         	// content, when they should be given content depending on their own personal info.
 			for(i = 0; i < index.length; i++) {
-    			var mytext = index[i] + "<br> Station No.: " + "<br> Free bikes: <br><a href='stations'>Click for more information.</a>" ;
+                var myUrl = "stations/"+i;
+    			
+                var mytext = index[i] + "<br> Station No.: " + "<br> Free bikes: <br>" + "<a id='popUp' href='#' onclick='poponclick();return false;'>Click for more details</a>";
+                
+               
+                
     			var myinfowindow = new google.maps.InfoWindow({content: mytext});
              
 				var position = new google.maps.LatLng(locations[index[i]].latitude, locations[index[i]].longitude);
