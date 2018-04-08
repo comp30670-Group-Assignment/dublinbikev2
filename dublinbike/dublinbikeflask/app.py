cda9cc29d6604e90216de0061709704dd9af1627
neuJump <35955724+neuJump@users.noreply.github.com>
2018-04-08 13:51:13 +0100

    Updated javascriptNew.js, adding in marker and infowindow code.

--- dublinbike/dublinbikeflask/app.py
+++ dublinbike/dublinbikeflask/app.py
@@ -1,13 +1,9 @@
 from flask import Flask, render_template, jsonify
 from dublinbikeflask.extractor import extractorv1
 import functools
+import os
 
 app = Flask(__name__)
-
-@app.route('/_map_data')
-def add_numbers():
-	recent = extractorv1.Extractor()
-	return jsonify(recent.getRecent())
 
 @app.route('/')
 @functools.lru_cache(maxsize=256)
@@ -20,7 +16,12 @@ def weather():
 
 @app.route('/stations')
 def stations():
-	return render_template('stations.html')
+	return render_template('stations.html', x=os.path.join(app.instance_path))
+
+@app.route('/_map_data')
+def add_numbers():
+	recent = extractorv1.Extractor()
+	return jsonify(recent.getRecent())
 
 if __name__ == '__main__':
 	app.run(debug=True)
