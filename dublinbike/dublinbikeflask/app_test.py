66096f3e8048a280091338ebe4094df2f424fd78
Hugh <hug_hob@hotmail.com>
2018-04-17 16:20:23 +0100

    added a file app_test that will resrun predictions at the beginning of everyday

--- dublinbike/dublinbikeflask/app_test.py
+++ dublinbike/dublinbikeflask/app_test.py
@@ -10,6 +10,14 @@ from app import predictions
 import time
 
 while True:
-    predictions("available_bike_stands")
     
-    time.sleep(24*60*60)
+    now = datetime.datetime.now()
+    hour = now.hour
+    minute = now.minute
+    second = now.second
+    
+    if hour == 1 and minute == 1 and second == 1:
+    
+        predictions("available_bike_stands")
+    
+        time.sleep((24*60*60) - 1)
