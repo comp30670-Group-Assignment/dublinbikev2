b8484f2a9bb5ba25ceab8bc8ad1628a06d33e039
Hugh <hug_hob@hotmail.com>
2018-04-17 09:34:28 +0100

    edits

--- dublinbike/dublinbikeflask/app.py
+++ dublinbike/dublinbikeflask/app.py
@@ -5,6 +5,7 @@ import os
 import sqlalchemy as sql
 import pandas as pd
 import datetime
+"""
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -25,6 +26,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score
 from sklearn.externals import joblib
+"""
 import pickle
 
 
