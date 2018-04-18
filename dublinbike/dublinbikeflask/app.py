b784a4b3062a81b6a4a1a8aa7d563fdef8806fe1
Hugh <hug_hob@hotmail.com>
2018-04-18 15:30:17 +0100

    edits

--- dublinbike/dublinbikeflask/app.py
+++ dublinbike/dublinbikeflask/app.py
@@ -146,15 +146,6 @@ def prediction_data(file_id):
 		predictionList.append(line);
 	return jsonify(predictionList);
 
-@app.route('/_predictions/<int:file_id>')
-def prediction_data(file_id):
-	predictionList = [];
-	file = "/static/predictions/test.json"
-	fileOpen  = file(file, "r");
-	for line in fileOpen:
-		predictionList.append(line);
-
-
 @functools.lru_cache(maxsize=256)
 @app.route('/_drop_data')
 def dropdown():
