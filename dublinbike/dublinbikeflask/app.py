764e99646dd0cb8d517438159968d4e6b1cb5d96
neuJump <35955724+neuJump@users.noreply.github.com>
2018-04-18 15:24:39 +0100

    Update extractor

--- dublinbike/dublinbikeflask/app.py
+++ dublinbike/dublinbikeflask/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from extractor import extractorv1
+from dublinbikeflask.extractor import extractorv1
 import functools
 import os
 import sqlalchemy as sql
@@ -145,15 +145,6 @@ def prediction_data(file_id):
 	for line in fileOpen:
 		predictionList.append(line);
 	return jsonify(predictionList);
-
-@app.route('/_predictions/<int:file_id>')
-def prediction_data(file_id):
-	predictionList = [];
-	file = "/static/predictions/test.json"
-	fileOpen  = file(file, "r");
-	for line in fileOpen:
-		predictionList.append(line);
-
 
 @functools.lru_cache(maxsize=256)
 @app.route('/_drop_data')
