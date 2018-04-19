fdeb7a51b847942f6c91153f13311fcab2456533
Hugh <hug_hob@hotmail.com>
2018-04-19 18:56:00 +0100

    edits

--- dublinbike/dublinbikeflask/navbar.js
+++ dublinbike/dublinbikeflask/navbar.js
@@ -1,7 +1,5 @@
 $.getJSON($SCRIPT_ROOT + '/_weather', function(data){
     
-    console.log(data);
-    
     $(document).ready(function(){
         $("#rightie-top").text(Math.round(data["0"]["temp"])+ '°C')
         $("#rightie-bottom").text(data["0"]["description"])
