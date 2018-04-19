fa5c8093931fd99798cd23afd644bf64088356ea
Hugh <hug_hob@hotmail.com>
2018-04-19 10:06:08 +0100

    edits

--- dublinbike/dublinbikeflask/navbar.js
+++ dublinbike/dublinbikeflask/navbar.js
@@ -2,5 +2,12 @@ $.getJSON($SCRIPT_ROOT + '/_weather', function(data){
     
     console.log(data);
     
-    
+    $(document).ready(function(){
+        $("#rightie-top").text(Math.round(data["0"]["temp"])+ '°C')
+        $("#rightie-bottom").text(data["0"]["description"])
+        var image = data["0"]["icon"]
+        $("#image").attr("src","http://openweathermap.org/img/w/" + image + ".png");
+             
+    });
+      
 });
